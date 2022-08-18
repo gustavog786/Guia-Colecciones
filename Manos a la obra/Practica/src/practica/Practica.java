f4dd895e7e3fe7d0b322593c685cf3d5777a59d9
Gustavo Godoy <gustavog786@gmail.com>
2022-08-18 18:19:34 -0300

    primer commit de la guia fecha 18/08/22

---
+++ Manos a la obra/Practica/src/practica/Practica.java
@@ -0,0 +1,19 @@
+
+package practica;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeSet;
+
+
+public class Practica {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArrayList<Integer> listado = new ArrayList();
+        TreeSet<String> conjunto = new TreeSet();
+        HashMap<Integer,String> personas = new HashMap();
+    }
+    
+}
